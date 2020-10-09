b9fd550b118c60cd7c687bd56bf07ff762d0bf99
Tiffany Brand <brand2learn@gmail.com>
2020-10-09 17:28:32 -0400

    Add ScrollReveal animations

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Scroll Reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+})
+
+sr.reveal('.about-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+})
+
+sr.reveal('.about-right', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px'
+})
+
+sr.reveal('.skill-reveal', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px'
+})
+
+sr.reveal('.portfolio-reveal', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px',
+    viewFactor: 0.2
+})
